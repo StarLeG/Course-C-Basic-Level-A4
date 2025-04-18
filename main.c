066055c91bc6ce395ccf245e86cee2c9a9d0c542
StarLeG <oleg.starynski@gmail.com>
2025-04-18 23:29:35 +0300

    Add answer

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 
 int main()
 {
-    printf("Hello World!\n");
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    printf("%d+%d+%d=%d\n", a, b, c, a + b + c);
+    printf("%d*%d*%d=%d\n", a, b, c, a * b * c);
+
     return 0;
 }
